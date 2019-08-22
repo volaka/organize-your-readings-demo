c76c546d72719eab864d0f0d203c44fc23fcec79
Volaka <hasancanvolaka@gmail.com>
2019-08-22 16:19:21 +0300

    Initial commit

--- src/components/InfoPane.js
+++ src/components/InfoPane.js
@@ -1,9 +1,7 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 import {connect} from "react-redux";
 import _ from 'lodash';
 import {Popup, Progress} from "semantic-ui-react";
-import renderHTML from 'react-render-html'
 class InfoPane extends Component {
 
   render() {
